ed9939e2c5a44fab73dbbe808e66f40ce4f44720
Belet <oguzhan.belet@solviads.com>
2022-06-30 23:40:46 +0300

    creates admin user

--- src/main/java/com/belet/springsecuritydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/belet/springsecuritydemo/security/ApplicationSecurityConfig.java
@@ -40,8 +40,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("STUDENT")  //ROLE_STUDENT
                 .build();
 
+        UserDetails lindaUser = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("password123"))
+                .roles("ADMIN")
+                .build();
+
         return new InMemoryUserDetailsManager(
-                annaSmithUser
+                annaSmithUser,
+                lindaUser
         );
     }
 }
