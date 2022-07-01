7bdf95b046f8619b3379e96681284b19b575fb60
Belet <oguzhan.belet@solviads.com>
2022-07-01 16:18:21 +0300

    adds Authorities to roles

--- src/main/java/com/belet/springsecuritydemo/security/ApplicationUserRole.java
+++ src/main/java/com/belet/springsecuritydemo/security/ApplicationUserRole.java
@@ -3,8 +3,11 @@ package com.belet.springsecuritydemo.security;
 import com.google.common.collect.Sets;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.belet.springsecuritydemo.security.ApplicationUserPermission.COURSE_READ;
 import static com.belet.springsecuritydemo.security.ApplicationUserPermission.COURSE_WRITE;
@@ -19,4 +22,12 @@ public enum ApplicationUserRole {
     ADMINTRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ));
 
     private final Set<ApplicationUserPermission> permissions;
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
+    }
 }
