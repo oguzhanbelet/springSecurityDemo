bc3f54a72e49484893e3067c62e096d1eed22184
Belet <oguzhan.belet@solviads.com>
2022-07-01 13:48:50 +0300

    fixes StudentManagementController endpoint

--- src/main/java/com/belet/springsecuritydemo/student/StudentManagementController.java
+++ src/main/java/com/belet/springsecuritydemo/student/StudentManagementController.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/students")
+@RequestMapping("management/api/v1/students")
 public class StudentManagementController {
 
     private static final List<Student> STUDENTS = Arrays.asList(
