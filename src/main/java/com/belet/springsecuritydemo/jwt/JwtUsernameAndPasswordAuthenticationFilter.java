dbf3e0136858454183d3a748b92fbf918e8a04c2
Belet <oguzhan.belet@solviads.com>
2022-07-05 19:03:45 +0300

    implements jwt filter and attemps Authentication

---
+++ src/main/java/com/belet/springsecuritydemo/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -0,0 +1,35 @@
+package com.belet.springsecuritydemo.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            UsernameAndPasswordAuthenticationRequest authenticationRequest =
+                    new ObjectMapper().readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(),
+                    authenticationRequest.getPassword()
+            );
+            Authentication authenticate = authenticationManager.authenticate(authentication);
+            return authenticate;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/belet/springsecuritydemo/jwt/UsernameAndPasswordAuthenticationRequest.java
@@ -0,0 +1,11 @@
+package com.belet.springsecuritydemo.jwt;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UsernameAndPasswordAuthenticationRequest {
+    private String username;
+    private String password;
+}
